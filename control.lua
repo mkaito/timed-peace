90462e0f848e6a171ee2c1b1a59400ac15a72a50
Michishige Kaito <me@mkaito.com>
2019-05-10 12:41:09 +0100

    Disable superfluous print statements

--- control.lua
+++ control.lua
@@ -1,19 +1,30 @@
+function printAll(text, bool)
+  if (debug or bool) then
+    local text2 = ""
+    if type(text) == "table" then text2 = serpent.block(text, {comment=false})
+      for i in pairs (game.players) do
+        game.players[i].print(text2)
+      end
+    else
+      for i in pairs (game.players) do
+        game.players[i].print(text)
+      end
+    end
+  end
+end
+
 script.on_nth_tick(3600, function(event)
   local elapsed = event.tick / 3600
   local timeout = settings.global["timed-peace-minutes"].value
-  local player = game.get_player(1)
   local nauvis = game.surfaces.nauvis
 
 
   if elapsed >= timeout then
     -- Disable peace
-    player.print("Peace timeout reached. Disabling peace.")
+    printAll("Peace timeout reached. Disabling peace.")
     nauvis.peaceful_mode = false
 
     -- Unregister handler
-    player.print("Disabling tick handler.")
     script.on_nth_tick(3600, nil)
-  else
-    player.print("Peace timeout not reached yet.")
   end
 end)
